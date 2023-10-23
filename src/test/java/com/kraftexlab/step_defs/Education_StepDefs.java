5c24da715f1ff1c7a9cb4ef85dac8179be1c188b
emrekcmn <emre.kcmn15@gmail.com>
2023-10-23 13:01:44 +0300

    Education_StepDefs class is added

---
+++ src/test/java/com/kraftexlab/step_defs/Education_StepDefs.java
@@ -0,0 +1,27 @@
+package com.kraftexlab.step_defs;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Education_StepDefs {
+    @When("Get user's all educations with GET method")
+    public void get_user_s_all_educations_with_GET_method() {
+
+    }
+
+    @When("Verify that there is NO any education information")
+    public void verify_that_there_is_NO_any_education_information() {
+
+    }
+
+    @When("Add education to user with POST method")
+    public void add_education_to_user_with_POST_method() {
+
+    }
+
+    @Then("Verify that education is added")
+    public void verify_that_education_is_added() {
+
+    }
+
+}
